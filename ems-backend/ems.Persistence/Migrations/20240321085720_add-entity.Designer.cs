8e3596076b031987a8d9b6342af5a1cb58e7b05f
chaitanya-bs <chaitanya.bigscal@gmail.com>
2024-03-21 14:30:56 +0530

    add: migration

--- ems-backend/ems.Persistence/Migrations/20240321085720_add-entity.Designer.cs
+++ ems-backend/ems.Persistence/Migrations/20240321085720_add-entity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ems.Persistence.Configuration;
@@ -11,9 +12,11 @@ using ems.Persistence.Configuration;
 namespace ems.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240321085720_add-entity")]
+    partial class addentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
